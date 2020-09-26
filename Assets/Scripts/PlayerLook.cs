ccc1a94f6f4933af08e68804597c6284287f6063
kkcoder69 <62224705+kkcoder69@users.noreply.github.com>
2020-09-25 21:15:07 -0400

    E

---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public float mouseSens = 200f;
+    public Transform playerBody;
+    float xRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 7f;
+    public float gravity = -25f;
+    public float groundDistance = 0.4f;
+    public float jumpHeight = 2f;
+
+    public Transform groundCheck;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+
+    bool isGrounded;
+    
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+        if (Input.GetButtonDown("LeftShift") && isGrounded)
+        {
+            speed = 14f;
+        }
+
+        if (Input.GetButtonUp("LeftShift") && isGrounded)
+        {
+            speed = 7f;
+        }
+
+        if (Input.GetButtonDown("crtl") && isGrounded)
+        {
+            speed = 3f;
+        }
+
+        if (Input.GetButtonUp("crtl") && isGrounded)
+        {
+            speed = 7f;
+        }
+    }
+}
